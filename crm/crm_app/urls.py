1b5cfb026448958e5186ab181f5a77ad243b49e2
Roman <ryzikov14@gmail.com>
2024-04-11 17:38:29 +0700

    0.0012

--- crm/crm_app/urls.py
+++ crm/crm_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('list-clients/', views.list_clients),
     path('list-clients/add-client/', views.add_client),
+    path('list-clients/<int:id>/', views.client_detail),
 ]
--- crm/crm_app/views.py
+++ crm/crm_app/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .forms import ClientForm
+from .models import Client
 
-def list_clients(request): 
-    return render(request,'list_clients.html')
+def list_clients(request):
+    clients = Client.objects.all()
+    context = {'clients':clients}
+    return render(request,'list_clients.html', context)
 
 def add_client(request):
-    return render(request,'add_client.html')
+    if request.method == 'POST':
+        form = ClientForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/list-clients/')
+    else:
+        form = ClientForm()
+
+    context = {'form':form}
+
+    return render(request,'add_client.html', context)
+
+def client_detail(request, id):
+    client = Client.objects.get(id=id)
+    context = {'client':client}
+    return render(request,'client_detail.html', context)
